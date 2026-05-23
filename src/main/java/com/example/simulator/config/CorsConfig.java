3cecdc4cf25469ca7ea87c3d708aa939efa1f2f5
Kushagra Gupta <kushagragupta@newgensoft.com>
2026-05-23 23:53:48 +0530

    updated cors config

--- src/main/java/com/example/simulator/config/CorsConfig.java
+++ src/main/java/com/example/simulator/config/CorsConfig.java
@@ -5,28 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of(
-        	    "http://localhost:3000",
-        	    "https://simulator-ui.vercel.app",
-        	    "https://simulator-ui-bokt.vercel.app"
-        	));
+        config.setAllowCredentials(false);
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
+        config.setExposedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-
         return new CorsFilter(source);
     }
 }
